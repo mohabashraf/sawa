15b21ede0cf4f6641a4f9754625de0eba9ef2c03
mohabashraf <mohabashraf466@gmail.com>
2019-12-06 02:19:08 +0200

    initial

---
+++ sawa/src/main/java/startup/sawa/SawaApplication.java
@@ -0,0 +1,13 @@
+package startup.sawa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SawaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SawaApplication.class, args);
+	}
+
+}
---
+++ sawa/src/test/java/startup/sawa/SawaApplicationTests.java
@@ -0,0 +1,13 @@
+package startup.sawa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SawaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
